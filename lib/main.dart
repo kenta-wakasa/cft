424f5535894230ee2ceeccb3859f3369b635fefe
Kenta Wakasa <wakasa.kenta.pg@gmail.com>
2024-08-05 11:24:20 +0900

    add ProviderScope

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,12 @@ import 'package:cft/firebase_options.dart';
 import 'package:cft/routes/my_app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(MyApp());
+  runApp(
+    ProviderScope(child: MyApp()),
+  );
 }
